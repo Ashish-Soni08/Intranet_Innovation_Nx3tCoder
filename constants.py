a8dd1bc844ced2c7afa9e8c3d805e3d53f1c28ef
Ashish Soni <ashish.soni2091@gmail.com>
2024-05-17 23:21:35 +0000

    add jina embedding model

--- constants.py
+++ constants.py
@@ -2,27 +2,28 @@ from dotenv import dotenv_values
 
 config = dotenv_values(".env")
 
-# API KEYS, URLS, TOKENS, MODEL NAMES ETC
+##### API KEYS, URLS, TOKENS, MODEL NAMES ETC #####
 
-## AZURE
+# Jina
+JINA_API_KEY: str = config["JINA_API"]
 
-AZURE_OPENAI_ENDPOINT: str = config["AZURE_OPENAI_ENDPOINT"]
-AZURE_OPENAI_API_KEY: str = config["AZURE_OPENAI_API_KEY"]
-API_VERSION: str = "2024-02-01"
-
-# ## LangChain(Langsmith)
+# # LangChain(Langsmith)
 # LANGCHAIN_API_KEY: str = config["LANGCHAIN_API"]
 # LANGCHAIN_URL: str = config["LANGCHAIN_ENDPOINT"]
 # LANGCHAIN_PROJECT: str = config["LANGCHAIN_PROJECT"]
 
-# ## Qdrant
-# QDRANT_API_KEY: str = config["QDRANT_API"]
-# QDRANT_CLUSTER: str = config["QDRANT_ENDPOINT"]
-
-# # MODELS
-
-EMBEDDING_MODEL: str = "text-embedding-3-small"
-
-LLM: str = "gpt-35-turbo"
-
+# OpenAI
 OPENAI_API_KEY: str = config["OPENAI_API"]
+
+
+# Qdrant
+QDRANT_CLUSTER: str = config["QDRANT_CLUSTER_URL"]
+QDRANT_API_KEY: str = config["QDRANT_API"]
+
+
+# MODELS
+
+EMBEDDING_MODEL: str =  "jina-embeddings-v2-base-de"
+
+
+LLM: str = "gpt-3.5-turbo-0125"
